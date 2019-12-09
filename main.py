66fc227f4a73f7623793be4cf2dfcccb8c5eeb4a
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-12-09 13:14:22 +0530

    feat: enhancement

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, Response, request as _request
 from dateparser import parse
 
@@ -12,7 +13,8 @@ def parse_date_route():
         except KeyError:
             return Response(
                 '{"message": "date query parameter not found", "success": false}',
-                mimetype='application/json', status=400,
+                mimetype="application/json",
+                status=400,
             )
 
         try:
@@ -20,16 +22,21 @@ def parse_date_route():
         except:
             return Response(
                 '{"message": "Parser was not able to parse the date", "success": false}',
-                mimetype='application/json', status=400,
+                mimetype="application/json",
+                status=400,
             )
-        r_msg = {'message': timestamp, 'success': True}
-        return Response(str(r_msg), mimetype='application/json', status=200)
+        r_msg = {"message": timestamp, "success": True}
+        return Response(str(r_msg), mimetype="application/json", status=200)
     except Exception as e:
         print(e)
         return Response(
-            '{"message": "Internal Server Error", "success": false}', mimetype='application/json', status=500
+            '{"message": "Internal Server Error", "success": false}',
+            mimetype="application/json",
+            status=500,
         )
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4587)
+    app.run(
+        host=os.getenv("HOST", "0.0.0.0"), port=os.getenv("PORT", 8000),
+    )
