e2046fe95081082eb13c9f2222af5f42dead0bff
Piyush Bhangale <bhangalepiyush@gmail.com>
2019-12-09 13:40:23 +0530

    use json.dumps

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, Response, request as _request
 from dateparser import parse
 
@@ -12,7 +13,9 @@ def parse_date_route():
             date = _request.args["date"]
         except KeyError:
             return Response(
-                '{"message": "date query parameter not found", "success": false}',
+                json.dumps(
+                    {"message": "date query parameter not found", "success": false}
+                ),
                 mimetype="application/json",
                 status=400,
             )
@@ -21,16 +24,24 @@ def parse_date_route():
             timestamp = parse(date).timestamp()
         except:
             return Response(
-                '{"message": "Parser was not able to parse the date", "success": false}',
+                json.dumps(
+                    {
+                        "message": "Parser was not able to parse the date",
+                        "success": false,
+                    }
+                ),
                 mimetype="application/json",
                 status=400,
             )
-        r_msg = {"message": timestamp, "success": True}
-        return Response(str(r_msg), mimetype="application/json", status=200)
+        return Response(
+            json.dumps({"message": timestamp, "success": True}),
+            mimetype="application/json",
+            status=200,
+        )
     except Exception as e:
         print(e)
         return Response(
-            '{"message": "Internal Server Error", "success": false}',
+            json.dumps({"message": "Internal Server Error", "success": false}),
             mimetype="application/json",
             status=500,
         )
